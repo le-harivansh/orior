b445acd2d6740e5df910c584283455fab8c96196
Harivansh <hello@harivan.sh>
2022-09-13 15:45:51 +0400

    feat: implement first working version of landing

--- src/Application.vue
+++ src/Application.vue
@@ -1,2 +1,16 @@
 <template>
+  <main class="w-full h-full grid place-items-center font-display">
+    <article class="p-1 flex flex-col space-y-10">
+      <section>
+        <p class="text-xl text-slate-400">hi, i'm</p>
+        <h2 class="font-heading text-9xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Harivansh</h2>
+        <p class="text-right text-xl text-slate-400">a web and software developer</p>
+      </section>
+
+      <section class="flex space-x-2 self-center">
+        <p class="text-xl text-slate-400">you can reach me at</p>
+        <a href="mailto:hello@harivan.sh" class="text-xl tracking-wide text-slate-800 border-b-2 border-slate-500">hello@harivan.sh</a>
+      </section>
+    </article>
+  </main>
 </template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,6 +7,10 @@ module.exports = {
     "./src/**/*.{vue,ts}",
   ],
   theme: {
+    fontFamily: {
+      display: ['Titillium Web', ...defaultTheme.fontFamily.sans],
+      heading: ['Poiret One', ...defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   plugins: [],
